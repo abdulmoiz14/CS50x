75b19ed7d8700566703a35d888fa0b982e92eba6
abdulmoiz14 <abdulmoiz14@users.noreply.github.com>
2020-09-02 16:19:08 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -9,14 +9,14 @@ while height < 1 or height > 8:
 for i in range(height):
     
     for space in range(height - i - 1):
-        print(" ", end = "")
+        print(" ", end="")
         
     for j in range(i + 1):
-        print("#", end = "")
+        print("#", end="")
         
-    print("  ", end = "")
+    print("  ", end="")
     
     for k in range(i+1):
-        print("#", end = "")
+        print("#", end="")
         
     print("")
