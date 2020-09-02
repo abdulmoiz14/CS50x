59936344d11777410cf747a0674a7bed14bac718
abdulmoiz14 <abdulmoiz14@users.noreply.github.com>
2020-09-02 16:17:01 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,21 +1,22 @@
 from cs50 import get_int
 
-rows = get_int("height: ")
+height = get_int("height: ")
 
-while height < 1 and height > 8:
-    rows = get_int("height: ")
+while height < 1 or height > 8:
+    height = get_int("height: ")
     
-for i in range(rows):
+
+for i in range(height):
     
-    for space in range(rows - i):
-        print(" ",end = "")
+    for space in range(height - i - 1):
+        print(" ", end = "")
         
     for j in range(i + 1):
-        print("#",end = "")
+        print("#", end = "")
         
-    print("  ",end = "")
+    print("  ", end = "")
     
     for k in range(i+1):
-        print("#",end="")
+        print("#", end = "")
         
     print("")
