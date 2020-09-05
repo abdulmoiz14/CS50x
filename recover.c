94daf38177ecccc2d3f85492e542f4dc28ae6fd7
abdulmoiz14 <abdulmoiz14@users.noreply.github.com>
2020-09-05 16:25:07 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -7,30 +7,34 @@ int main(int argc, char *argv[])
 	int blocksize=512;
 	char filename[8];
 	int counter=0;
+	bool jpeg_found = false;
+	FILE* output=NULL;
 	FILE *input = fopen(argv[1], "r");
 	if(input == NULL)
 	{
 	    printf("Command Line Argument Error!");
 	    return 1;
 	}
-	while(!feof(input))
+	while(fread(buffer,512,1,input))
 	{
-		fread(buffer,512,1,input);
 		if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff )
 	    {
+	    	if(jpeg_found==true)
+	    	{
+	    		fclose(output);
+	    	}
+	    	else
+	    	{
+	    		jpeg_found=true;
+	    	}
 	 		sprintf(filename, "%03i.jpg", counter);
         	counter ++;
-			FILE* output=fopen(filename,"wb");
+			output=fopen(filename,"w");
 			fwrite(buffer,512,1,output);
-			do
-			{
-				fread(buffer,512,1,input);
-				fwrite(buffer,512,1,output);
-			}while((buffer[0] == 0xff &&
-                	buffer[1] == 0xd8 &&
-                	buffer[2] == 0xff &&
-                   (buffer[3] & 0xf0) == 0xe0) == 0);
-			fclose(output);
+		}
+		else if(jpeg_found==true)
+		{
+			fwrite(buffer,512,1,output);
 		}
 	}
 	fclose(input);
